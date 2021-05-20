b8204bb4537eaf254a0927329391b8fb0ae9ffaa
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-05-20 19:43:06 -0300

    Implementacao micro serviço worker

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -2,10 +2,12 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HrWorkerApplication {
 
+	@GetMapping
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
 	}
