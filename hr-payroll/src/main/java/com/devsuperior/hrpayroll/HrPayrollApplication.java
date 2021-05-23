80d0075122752cd30e4d97cb6f55b247242aa26a
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-05-23 12:09:57 -0300

    Ribbon add

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,10 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
@@ -11,10 +13,5 @@ public class HrPayrollApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrPayrollApplication.class, args);
 	}
-<<<<<<< HEAD
 
 }
-=======
-	
-}
->>>>>>> be29caf9de99478e3c76dd37662b495e6670a989
